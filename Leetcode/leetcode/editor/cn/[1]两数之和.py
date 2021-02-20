dd2f61d26728e41c8634cb7d5cb2ead8a3a66827
Fred Wang <1107943410@qq.com>
2021-02-21 02:21:59 +0800

    Add some leetcode file

--- Leetcode/leetcode/editor/cn/[1]两数之和.py
+++ Leetcode/leetcode/editor/cn/[1]两数之和.py
@@ -43,6 +43,54 @@
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         output = list()
+#         import copy
+#         for num_1 in nums:
+#             num_temp = target - num_1
+#             num_1_index = nums.index(num_1)
+#             list_temp = nums.copy()
+#             list_temp.pop(num_1_index)
+#             if num_temp in list_temp:
+#                 output.append(num_1_index)
+#                 if num_1 != num_temp:
+#                     num_temp_index = nums.index(num_temp)
+#                     output.append(num_temp_index)
+#                     break
+#                 else:
+#                     num_temp_index = [i for i,x in enumerate(nums) if x==num_temp][1]
+#                     output.append(num_temp_index)
+#                     break
+#         return output
+# 耗时与内存久一点，需要载入copy这个库
+# 此处的暴力法需要注意的是，copy是浅copy，b = a 传递的list的内存地址是不变的
+# list.copy对象是新的内存地址，此处用空间换时间，list的搜索算法复杂度是nlogn
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        output = list()
+        for num_1 in nums:
+            num_temp = target - num_1
+            num_1_index = nums.index(num_1)
+            list_temp = nums.copy()
+            list_temp.pop(num_1_index)
+            if num_temp in list_temp:
+                output.append(num_1_index)
+                if num_1 != num_temp:
+                    num_temp_index = nums.index(num_temp)
+                    output.append(num_temp_index)
+                    break
+                else:
+                    num_temp_index = [i for i,x in enumerate(nums) if x==num_temp][1]
+                    output.append(num_temp_index)
+                    break
+        return output
+
+# 13:08	info: 已提交,请稍等
+#
+# 13:08	info
+# 			解答成功:
+# 			执行耗时:32 ms,击败了95.94% 的Python3用户
+# 			内存消耗:15 MB,击败了24.97% 的Python3用户
 # leetcode submit region end(Prohibit modification and deletion)
